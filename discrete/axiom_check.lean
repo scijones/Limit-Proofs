e1eb2290b321df0efee2437e9429e93b1fc30733
scijones <scijones@umich.edu>
2026-07-02 10:10:28 -0400

    Add kernel axiom-footprint regression check (axiom_check.lean)

---
+++ discrete/axiom_check.lean
@@ -0,0 +1,7 @@
+import AsymptoticBB
+
+#print axioms full_behavior_bound
+#print axioms main_theorem_finite_subclass
+#print axioms main_theorem_full
+#print axioms full_behavior_bound_nonvacuous
+#print axioms flagAgent_behavior_mem
